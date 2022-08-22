1b82e7cc6093353507f8f557295c6a1ebc5faabe
Flordeliza Mae Canlas <maemaeperlas@gmail.com>
2022-08-21 19:56:18 -0700

    commit tree 0b58b3ed6b5c2e34b69f9935cb8a893dc22ff36d

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,26 @@
 class ApplicationController < Sinatra::Base
 
   # add routes
+  set :default_content_type, "application/json"
+
+  get "/bakeries" do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
+
+  get "/bakeries/:id" do
+    bakeries = Bakery.find(params[:id])
+    bakeries.to_json(include: :baked_goods)
+  end
+
+  get "/baked_goods/by_price" do
+    bakeries = BakedGood.order(price: :desc)
+    bakeries.to_json(only: [:name, :price])
+  end
+
+  get "/baked_goods/most_expensive" do
+    bakeries = BakedGood.order(price: :desc).first
+    bakeries.to_json(only: [:name, :price])
+  end
 
 end
